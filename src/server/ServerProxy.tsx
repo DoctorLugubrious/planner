bbfe88cf728881f0899415a7869da0d57063ad11
green_admin <green.avery.s@gmail.com>
2020-03-17 15:58:41 -0600

    changed to https

--- src/server/ServerProxy.tsx
+++ src/server/ServerProxy.tsx
@@ -17,7 +17,7 @@ export default class ServerProxy {
 	async sendRequest(path: string, info: any) {
 		return request({
 			        "method":"POST",
-			        "uri": "http://averys.green:3001/" + path,
+			        "uri": "https://plan.averys.green:3001/" + path,
 			        "json": true,
 			        "body": info
 		        })
